853fcc3554da720bb9dc7b01e63450bc306a8fff
Nathan Neve <nathanneve@live.be>
2024-12-28 12:51:15 +0100

    env loading in app

--- code/labflow-backend/src/main/java/com/thomasmore/blc/labflow/LabflowApplication.java
+++ code/labflow-backend/src/main/java/com/thomasmore/blc/labflow/LabflowApplication.java
@@ -2,13 +2,21 @@ package com.thomasmore.blc.labflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import io.github.cdimascio.dotenv.Dotenv;
 
 
 @SpringBootApplication
 public class LabflowApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load(); // Load .env file
+		System.setProperty("env.DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
+		System.setProperty("env.DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+		System.setProperty("env.USER_ADMIN_PASSWORD", dotenv.get("USER_ADMIN_PASSWORD"));
+		System.setProperty("env.USER_CESAR_PASSWORD", dotenv.get("USER_CESAR_PASSWORD"));
+		System.setProperty("env.USER_NATHAN_PASSWORD", dotenv.get("USER_NATHAN_PASSWORD"));
+
 		SpringApplication.run(LabflowApplication.class, args);
 	}
 }
